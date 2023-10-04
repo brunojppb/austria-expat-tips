b9d94d02251c7ad1249ba223737f2577539098eb
Bruno Paulino <bruno@bpaulino.com>
2023-10-04 23:21:31 +0200

    Nextra website setup (#7)

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Austria Expat Tips</span>,
+  project: {
+    link: "https://github.com/brunojppb/austria-expat-tips",
+  },
+  docsRepositoryBase: "https://github.com/brunojppb/austria-expat-tips",
+  footer: {
+    text: "Austria Expact Tips",
+  },
+};
+
+export default config;
