79aa27126d7de5936dd092a205b2630b837951a1
Bruno Paulino <bruno@bpaulino.com>
2023-10-06 23:45:21 +0200

    Add favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,6 +10,29 @@ const config: DocsThemeConfig = {
   footer: {
     text: "Austria Expact Tips",
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Austria Expat Tips" />
+      <meta
+        property="og:description"
+        content="Tips for expats living in Austria"
+      />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </>
+  ),
   useNextSeoProps: () => {
     return {
       title: "Austria Expat Tips",
